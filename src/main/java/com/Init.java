7df7903a3e924eef85e195115085fa322bccb5bc
dy119590 <dy119590@alibaba-inc.com>
2017-11-24 20:17:42 +0800

    配置中心化----服务端

--- src/main/java/com/Init.java
+++ src/main/java/com/Init.java
@@ -1,4 +1,4 @@
-package com.example;
+package com;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
--- src/main/java/com/MyConfigServerApplication.java
+++ src/main/java/com/MyConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package com;
 
 import org.springframework.boot.SpringApplication;
 
