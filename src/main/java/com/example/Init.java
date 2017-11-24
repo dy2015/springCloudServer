4b68b7a4273b1a40d11fef970aa489496e19cce3
dy119590 <dy119590@alibaba-inc.com>
2017-11-24 20:16:33 +0800

    配置中心化----服务端

---
+++ src/main/java/com/example/Init.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Init extends SpringBootServletInitializer {
+	//容器启动时，自动初始化
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Init.class);
+	}
+}
---
+++ src/main/java/com/example/MyConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+
+//@SpringBootApplication
+//@EnableConfigServer
+public class MyConfigServerApplication {
+//手动启动
+	public static void main(String[] args) {
+		SpringApplication.run(MyConfigServerApplication.class, args);
+	}
+}
